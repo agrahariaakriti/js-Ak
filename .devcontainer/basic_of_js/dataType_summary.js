bae7a0e2b4515ac7b5e04ff8aa65ff1493992244
agrahariaakriti <114635032+agrahariaakriti@users.noreply.github.com>
2024-07-28 14:00:14 +0000

    DataType_Summay

---
+++ .devcontainer/basic_of_js/dataType_summary.js
@@ -0,0 +1,20 @@
+// There are two type od data i.e primitive and non primitive
+let id=Symbol('123');
+let anotherid=Symbol('123');
+console.log(id===anotherid);//false
+const bigInt=123456789077987993847975n;
+console.log(bigInt);
+
+// non primitive 
+let a=[12,33,'y',"u"];
+console.log(a);
+
+let fun=function()
+{
+    return 2;
+}
+console.log(fun());
+
+
+TypeOf   
+
