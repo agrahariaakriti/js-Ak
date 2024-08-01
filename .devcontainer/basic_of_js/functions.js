2fb8cebe15cfc640fe5e0d92f2e8f46f2d0e6ee8
agrahariaakriti <114635032+agrahariaakriti@users.noreply.github.com>
2024-08-01 06:08:47 +0000

    Functions

---
+++ .devcontainer/basic_of_js/functions.js
@@ -0,0 +1,22 @@
+// question 1
+const marks=
+{
+    "marks":94,
+    "science":98,
+    "english":93,
+    "Chemistry":91,
+};
+for(let m in marks)
+{
+    console.log(m," : ",marks.m);
+}
+// question 2
+no1=35;
+// do{
+// // let no =prompt("enter the ocrrect no");
+
+// }while(no!=no1);
+let s="aakriti";
+s=s+"agrahari";
+console.log(s);
+console.log(s);
