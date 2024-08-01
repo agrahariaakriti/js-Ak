9ac91a51fd202db4af4c0cfd301056a1ac5fb4bf
agrahariaakriti <114635032+agrahariaakriti@users.noreply.github.com>
2024-08-01 06:08:29 +0000

    String and functions

---
+++ .devcontainer/basic_of_js/string.js
@@ -0,0 +1,8 @@
+// question 1
+console.log("aakriti \' mahana \' hai ");
+//  q   uestion 2
+let s='Aakriti Agrahari';
+console.log(s.toUpperCase());
+console.log(s.toLowerCase());
+let str="please give Rs 1000";
+console.log(s.replace())
